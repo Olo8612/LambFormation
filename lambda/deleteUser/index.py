854c1d1de76cee3cb19fdde2dc3f49b29d26f638
Olo8612 <aleksander.rekawek@gmail.com>
2020-01-28 22:42:14 +0100

    add common layer as resource in main cfn template, some adjustments inside

--- lambda/deleteUser/index.py
+++ lambda/deleteUser/index.py
@@ -1,11 +1,15 @@
 import boto3
 import os
+from cleanUpUserGroups import cleanUpUser
 
 cfClient = boto3.client('cloudformation')
 USER_STACK_PREFIX = os.environ['USER_STACK_PREFIX']
 
 def lambdaHandler(event, context):
     userToDelete = event['UserName']
+    
+    cleanUpUser(userToDelete)
+    
     userStack = f'{USER_STACK_PREFIX}-{userToDelete}'
     cfClient.delete_stack(
         StackName=userStack
